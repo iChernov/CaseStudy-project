229326ab041e76879cde9782daac0d0ae1ffa23b
Ivan Chernov <ichernov@me.com>
2014-07-08 11:07:07 +0200

    Initial commit

---
+++ CaseStudy/CaseStudy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CaseStudy
+//
+//  Created by Ivan Chernov on 08/07/14.
+//  Copyright (c) 2014 iC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CaseStudy/CaseStudy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CaseStudy
+//
+//  Created by Ivan Chernov on 08/07/14.
+//  Copyright (c) 2014 iC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
